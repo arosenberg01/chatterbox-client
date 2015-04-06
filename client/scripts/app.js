d5a133a19445ae44b9799172d40d4122df9a5ce7
arosenberg01 <ansel01@gmail.com>
2015-04-06 15:31:12 -0700

    Add message fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,24 @@
 // YOUR CODE HERE:
-var username = prompt("Username:");
-var message = {};
+var username =  'anselandhenry'        // prompt("Username:");
+/*
+
+
 
 message.username = username;
 message.roomname = "4chan";
+
+
+*/
+var message = {};
+message.roomname = "4chan";
+message.username = username;
 
 $(function() {
 
   $('#send').on('click', function() {
     message.text = $('#message').val();
-    app.send();
+    app.send(message);
+    app.addMessage(message.text)
   });
 
 
@@ -23,17 +32,41 @@ app.init = function() {
 
 };
 
-app.send = function() {
+app.server = "https://api.parse.com/1/classes/chatterbox";
+
+app.send = function(message) {
   $.ajax({
     url: "https://api.parse.com/1/classes/chatterbox",
     type: "POST",
     data: JSON.stringify(message),
     contentType: "application/json",
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message sent' + data);
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
     }
   });
 };
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    type: "GET",
+    contentType: "application/json",
+    success: function (data) {
+      console.log('chatterbox: fetching');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed');
+    }
+  });
+}
+
+app.clearMessages = function() {
+  $('#chats').empty();
+}
+
+app.addMessage = function(message) {
+  $('#chats').append('<span>' +message+ '</span>');
+}
