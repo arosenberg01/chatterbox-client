7c70e22bb50bba6927a28a92d52e5da2d545cc6e
arosenberg01 <ansel01@gmail.com>
2015-04-06 15:05:51 -0700

    Add send function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,39 @@
 // YOUR CODE HERE:
+var username = prompt("Username:");
+var message = {};
 
+message.username = username;
+message.roomname = "4chan";
+
+$(function() {
+
+  $('#send').on('click', function() {
+    message.text = $('#message').val();
+    app.send();
+  });
+
+
+});
+
+
+var app = {};
+
+app.init = function() {
+
+
+};
+
+app.send = function() {
+  $.ajax({
+    url: "https://api.parse.com/1/classes/chatterbox",
+    type: "POST",
+    data: JSON.stringify(message),
+    contentType: "application/json",
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
